6b40886faf073c47dc012abb8ab447cef065cd5a
Šimun Strukan <simun.strukan@gmail.com>
2017-08-07 22:34:47 +0200

    Added manager classes to ApiHelpers for managing requested tables and API request

--- controllers/records.rb
+++ controllers/records.rb
@@ -8,129 +8,144 @@ class RecordsController < Sinatra::Base
 
 	before do
 		content_type :json
-		#error 400 unless valid_request?
+		error 400 unless @request.media_type == 'application/json'
 	end
 
 	#Fetching
-	get '/:model' do 
-		halt(api_error 1001) unless valid_model?	
-		records ||= model.all() || halt(api_error 1001)
-		api_response records, model
+	get '/:table' do 
+		@api_manager = APIManager.new request, params
+		api_error(1001) unless @api_manager.table.valid?
+		records ||= @api_manager.table.model.all() || api_error(1002)
+		@api_manager.respond :with => records
 	end
 	
-	get '/:model/:id' do 
-		halt(api_error 1001) unless valid_model?	
-		record ||= model.get(record_id) || halt(api_error 1002)
-		api_response record, model
+	get '/:table/:id' do
+		@api_manager = APIManager.new request, params
+		api_error(1001) unless @api_manager.table.valid?
+		record ||= @api_manager.table.model.get(@api_manager.table.id) || api_error(1002)
+		api_manager.respond :with => record
 	end
 	
-	get '/:model/:id/:child_model' do		
-		halt(api_error 1001) unless valid_model? && valid_child_model?	
-		parent_record ||= model.get(record_id) || halt(api_error 1002)
-		records ||= parent_record.send child_model_method || halt(api_error 1002)
-		api_response records, child_model
+	get '/:table/:id/:child_table' do
+		@api_manager = APIManager.new request, params
+		api_error(1001) unless @api_manager.table.valid? && @api_manager.child_table.valid?
+		parent_record ||= @api_manager.table.model.get(@api_manager.table.id) || api_error(1002)
+		records ||= parent_record.send @api_manager.child_table.name || api_error(1002)
+		api_manager.respond :with => records
 	end
 	
-	get '/:model/:id/:child_model/:child_id' do 
-		halt(api_error 1001) unless valid_model? && valid_child_model?
-		parent_record ||= model.get(record_id) || halt(api_error 1002)
+	get '/:table/:id/:child_table/:child_id' do
+		@api_manager = APIManager.new request, params
+		api_error(1001) unless @api_manager.table.valid? && @api_manager.child_table.valid?
+		parent_record ||= @api_manager.table.model.get(@api_manager.table.id) || api_error(1002)
 		record ||= parent_record.send(child_model_method).get(child_id) || halt(api_error 1002)
-		api_response record, child_model
+		@api_manager.respond :with => record
 	end
 
-	get '/:model/:id/:join_model/:join_id/:child_model' do
-		halt(api_error 1001) unless valid_model? && valid_join_model? && valid_child_model?
-		parent_record ||= model.get(record_id) || halt(api_error 1002)
-		record ||= parent_record.send(join_model_method).get(join_id).send(child_model_method) || halt(api_error 1002)
-		api_response record, child_model
+	get '/:table/:id/:join_table/:join_id/:child_table' do
+		@api_manager = APIManager.new request, params
+		api_error(1001) unless @api_manager.table.valid? && @api_manager.child_table.valid? && @api_manager.join_table.valid?
+		parent_record ||= @api_manager.table.model.get(record_id) || api_error(1002)
+		records ||= parent_record.send(@api_manager.join_table.name).get@api_manager.join_table.id).send(@api_manager.child_table.name) || api_error(1002)
+		@api_manager.respond :with => records
 	end
 
-	get '/:model/:id/:join_model/:join_id/:child_model/:child_id' do
-		halt(api_error 1001) unless valid_model? && valid_join_model? && valid_child_model?
-		parent_record ||= model.get(record_id) || halt(api_error 1002)
-		record ||= parent_record.send(join_model_method).get(join_id).send(child_model_method).get(child_id) || halt(api_error 1002)
-		api_response record, child_model
+	get '/:table/:id/:join_table/:join_id/:child_table/:child_id' do
+		@api_manager = APIManager.new request, params
+		api_error(1001) unless @api_manager.table.valid? && @api_manager.child_table.valid? && @api_manager.join_table.valid?
+		parent_record ||= @api_manager.table.model.get(record_id) || api_error(1002)
+		record ||= parent_record.send(@api_manager.join_table.name).get(@api_manager.join_table.id).send(@api_manager.child_table.name).get(@api_manager.child_table.id) || api_error(1002)
+		@api_manager.respond :with => record
 	end
 
 	#Creating 
-	post '/:model' do
-		halt(api_error 1001) unless valid_model?
-		new_record = model.new
-		new_record.attributes = api_request[:json_body]
-		halt(api_error 1003, new_record) unless new_record.save
-		api_response new_record, model
+	post '/:table' do
+		@api_manager = APIManager.new request, params
+		api_error(1001) unless api_manager.table.valid?
+		new_record = @api_manager.table.model.new
+		new_record.attributes = @api_manager.json_body
+		api_error(1003, new_record.errors) unless new_record.save
+		@api_manager.respond :with => new_record
 	end
 	
-	post '/:model/:id/:child_model' do
-		halt(api_error 1001) unless valid_model? && valid_child_model?
-		parent_record ||= model.get(record_id) || halt(api_error 1002)
-		new_record = parent_record.send(child_model_method).new
-		new_record.attributes = api_request[:json_body]
-		halt(api_error 1003, parent_record) unless parent_record.save
-		api_response new_record, model
+	post '/:table/:id/:child_table' do
+		@api_manager = APIManager.new request, params
+		api_error(1001) unless @api_manager.table.valid? && @api_manager.child_table.valid?
+		parent_record ||= @api_manager.table.model.get(@api_manager.table.id) || api_error(1002)
+		new_record = parent_record.send(@api_manager.child_table.name).new
+		new_record.attributes = @api_manager.json_body
+		api_error(1003, parent_record.errors) unless parent_record.save
+		@api_manager.respond :with => new_record
 	end
 
-	post '/:model/:id/:join_model/:child_model' do
-		halt(api_error 1001) unless valid_model? && valid_join_model? && valid_child_model?
-		parent_record ||= model.get(record_id) || halt(api_error 1002)
-		join_record = parent_record.send(join_model_method).new 
-		new_record = child_model.new
-		new_record.attributes = api_request[:json_body]
-		new_record.send(join_model_method) << join_record
-		halt(api_error 1003, parent_record) unless parent_record.save
-		api_response new_record, child_model
+	post '/:table/:id/:join_table/:child_table' do
+		@api_manager = APIManager.new request, params
+		api_error(1001) unless @api_manager.table.valid? && @api_manager.child_table.valid? && @api_manager.join_table.valid?
+		parent_record ||= @api_manager.table.model.get(@api_manager.table.id) || api_error(1002)
+		join_record = parent_record.send(@api_manager.join_table.name).new 
+		new_record = @api_manager.child_table.model.new
+		new_record.attributes = @api_manager.json_body
+		new_record.send(@api_manager.join_table.name) << join_record
+		api_error(1003, parent_record.errors) unless parent_record.save
+		@api_manager.respond :with => new_record
 	end
 
 	#Updating 
-	put '/:model/:id' do
-		halt(api_error 1008) unless valid_model?
-		record ||= model.get(record_id) || halt(api_error 1001)
-		record.attributes = api_request[:json_body]
-		halt(api_error 1003, record) || record.save
-		api_response record, model
+	put '/:table/:id' do
+		@api_manager = APIManager.new request, params
+		api_error(1001) unless @api_manager.table.valid?
+		record ||= @api_manager.table.model.get(@api_manager.table.id) || api_error(1002)
+		record.attributes = @api_manager.json_body
+		api_error(1003, record.errors) || record.save
+		@api_manager.respond :with => record
 	end
 	
-	put '/:model/:id/:child_model/:child_id' do 
-		halt(api_error 1008) unless valid_model? && valid_child_model?
-		parent_record ||= model.get(record_id) || halt(api_error 1001)
-		record ||= parent_record.send(child_model_name).get(child_id) || halt(api_error 1001)
-		record.attributes = api_request[:json_body]
-		halt(api_error 1003, record) unless record.save
-		api_response record, child_model
+	put '/:table/:id/:child_table/:child_id' do 
+		@api_manager = APIManager.new request, params
+		api_error(1001) unless @api_manager.table.valid? && @api_manager.child_table.valid?
+		parent_record ||= @api_manager.table.model.get(@api_manager.table.id) || api_error(1001)
+		record ||= parent_record.send(@api_manager.child_table.name).get(@api_manager.child_table.id) || api_error(1001)
+		record.attributes = @api_manager.json_body
+		api_error(1003, record.errors) unless record.save
+		@api_manager.respond :with => record
 	end
 
-	put '/:model/:id/:join_model/:join_id/:child_model/:child_model_id' do
-		halt(api_error 1008) unless valid_model? && valid_join_model? && valid_child_model?
-		parent_record ||= model.get(record_id) || halt(api_error 1001)
-		join_record ||= parent_record.send(join_model_method).get(join_id) || halt(api_error 1001)
-		record ||= join_record.send(child_model_method).get(child_model_id) || halt(api_error 1001)
-		record.attributes = api_request[:json_body]
-		halt(api_error 1003, record) unless record.save
-		api_response record, child_model
+	put '/:table/:id/:join_table/:join_id/:child_table/:child_id' do
+		@api_manager = APIManager.new request, params
+		api_error(1001) unless @api_manager.table.valid? && @api_manager.child_table.valid? && @api_manager.join_table.valid?
+		parent_record ||= @api_manager.table.model.get(@api_manager.table.id) || api_error(1002)
+		join_record ||= parent_record.send(@api_manager.join_table.name).get(@api_manager.join_table.id) || api_error(1002)
+		record ||= join_record.send(@api_manager.child_table.name).get(@api_manager.child_table.id) || api_error(1002)
+		record.attributes = @api_manager.json_body
+		api_error(1003, record.errors) unless record.save
+		@api_manager.respond :with => record
 	end
 
 	#Deleting
-	delete '/:model/:id' do
-		halt(api_error 1008) unless valid_model?
-		record ||= model.get(record_id) || halt(api_error 1001)
-		halt(api_error 1004) unless record.destroy
+	delete '/:table/:id' do
+		@api_manager = APIManager.new request, params
+		api_error(1001) unless @api_manager.table.valid?
+		record ||= @api_manager.table.model.get(@api_manager.table.id) || api_error(1002)
+		api_error(1004) unless record.destroy
 		{:deleted => true}.to_json
 	end
 	
-	delete '/:model/:id/:child_model/:child_id' do
-		halt(api_error 1008) unless valid_model? && valid_child_model?
-		parent_record ||= model.get(record_id) || halt(api_error 1001)
-		record ||= parent_record.send(child_model_name).get(child_id)
-		halt(api_error 1004) unless record.destroy
+	delete '/:table/:id/:child_table/:child_id' do
+		@api_manager = APIManager.new request, params
+		api_error(1001) unless @api_manager.table.valid? && @api_manager.child_table.valid?
+		parent_record ||= @api_manager.table.model.get(@api_manager.table.id) || api_error(1002)
+		record ||= parent_record.send(@api_manager.child_table.name).get(@api_manager.child_table.id)
+		api_error(1004) unless record.destroy
 		{:deleted => true}.to_json
 	end
 
-	delete '/:model/:id/:join_model/:join_id/:child_model/:child_mode_id' do
-		halt(api_error 1008) unless valid_model? && valid_join_model? && valid_child_model?
-		parent_record ||= model.get(record_id) || halt(api_error 1001)
-		join_record ||= parent_record.send(join_model_method).get(join_id) || halt(api_error 1001)
-		record ||= join_record.send(child_model_method).get(child_model_id) || halt(api_error 1001)
-		halt(api_error 1004) unless record.destroy
+	delete '/:table/:id/:join_table/:join_id/:child_table/:child_id' do
+		@api_manager = APIManager.new request, params
+		api_error(1001) unless @api_manager.table.valid? && @api_manager.child_table.valid? && @api_manager.join_table.valid?
+		parent_record ||= @api_manager.table.model.get(@api_manager.table.id) || api_error(1002)
+		join_record ||= parent_record.send(@api_manager.join_table.name).get(@api_manager.join_table.id) || api_error(1002)
+		record ||= join_record.send(@api_manager.child_table.name).get(@api_manager.child_table.id) || api_error(1002)
+		api_error(1004) unless record.destroy
 		{:deleted => true}.to_json
 	end
 
--- helpers/api_request.rb
+++ helpers/api_request.rb
@@ -2,107 +2,65 @@ require 'sinatra/base'
 
 module Sinatra
 	module ApiRequestHelper
+		
+		class APIManager
+			attr_accessor :table, :child_table, :join_table
 
-		def api_request
-			body = nil
-			method = request.request_method
-			unless method == 'GET' || method == 'DELETE'
-				request.body.rewind
-				body = ::JSON.parse(request.body.read)
+			def initialize request, params
+				@request = request
+				@params = params
+				@table = TableManager.new @params[:table], @params[:id] unless @params[:table].nil?
+				@child_table = TableManager.new @params[:child_model], @params[:child_id] unless @params[:child_model].nil?
+				@join_table = TableManager.new @params[:join_model], @params[:join_id] unless @params[:join_model].nil?
 			end
-			{:request => request, :params => params, :json_body => body}
+
+			def json_body
+				body = nil
+				if @request.content_lenght > 0
+					@request.body.rewind
+					body = ::JSON.parse(@request.body.read)
+				end
+				body
+			end
+
+			def respond data
+				data = data[:with]
+				expand = @params[:expand] == "1"		
+				model_methods = @child_table.nil? ? @table.relationships : @child_table.relationships
+				response = expand ? data.to_json(:methods => model_methods) : data.to_json
+				response
+			end
+
+		end
+		
+		class TableManager
+			attr_accessor :table_name, :model_name, :id
+
+			def initialize requested_table, record_id
+				@name = requested_table
+				@model_name = @name.capitalize[0...-1]
+				@id ||= record_id
+			end
+
+			def valid?
+				Object.const_defined? @model_name
+			end
+
+			def model
+				Object.const_get @model_name
+			end
+
+			def relationships
+				methods = []
+				model.relationships.each do |relationship|
+					method = relationship.name.to_sym
+					methods << method
+				end	
+				methods
+			end
 		end
 
-		def api_response data, model
-			envelope = api_request[:params][:envelope] == "1"  
-			expand = api_request[:params][:expand] == "1"
-			model_methods = relations :for => model
-			response = data.to_json
-			response = data.to_json(:methods => model_methods) if expand
-			# response = {:data => data}.to_json if envelope
-			# response = {:data => data}.to_json(:methods => model_methods) if envelope && expand
-			response
-		end
-
-		def relations data
-			model = data[:for]
-			methods = []
-			model.relationships.each do |relationship|
-				method = relationship.name.to_sym
-				methods << method
-			end	
-			methods
-		end
-
-		def valid_media_type?
-			request.media_type == 'application/json'
-		end
-				
-		def record_id
-			api_request[:params][:id]
-		end
-
-		def model_name
-			api_request[:params][:model].capitalize[0...-1]
-		end
-
-		def valid_model?
-			Object.const_defined? model_name
-		end
-
-		def model
-			Object.const_get(model_name)
-		end
-
-		def child_id
-			api_request[:params][:child_id]
-		end
-
-		def child_model_property_name
-			child_model_method[0...-1]
-		end
-
-		def child_model_method
-			api_request[:params][:child_model]
-		end
-
-		def child_model_name
-			api_request[:params][:child_model].capitalize[0...-1]
-		end
-
-		def valid_child_model?
-			Object.const_defined? child_model_name
-		end
-
-		def child_model
-			Object.const_get child_model_name
-		end
-
-		def join_id 
-			api_request[:params][:join_id]
-		end
-
-		def join_model_method
-			api_request[:params][:join_model]
-		end
-
-		def join_belongs_to
-			child_model_property_name
-		end
-
-		def join_model_name 
-			api_request[:params][:join_model].capitalize[0...-1]
-		end
-
-		def join_model
-			Object.const_get join_model_name
-		end
-
-		def valid_join_model?
-			Object.const_defined? join_model_name 
-		end
-
-		def api_error code, model = nil
+		def api_error code, model_errors = nil, halt_request = true
 			errors = [
 				{
 					:code => 1001,
@@ -121,20 +79,8 @@ module Sinatra
 					:msg => "Record not deleted. Requested record can not be found."
 				},
 				{
-					:code => 1005,
-					:msg => "Provided email is in use by another user."
-				},
-				{
 					:code => 1006,
 					:msg => "Invalid email and password combination."
-				},
-				{
-					:code => 1007,
-					:msg => "There is no user with provided email in our records."
-				},
-				{
-					:code => 1008,
-					:msg => "Invalid model request."
 				},
 				{
 					:code => 1009,
@@ -144,14 +90,19 @@ module Sinatra
 
 			error_msg = errors.find {|error| error[:code] == code}
 
-			unless model.nil?
+			unless model_errors.nil?
 				error_msg[:desc] = []
-				model.errors.each do |error|
+				model_errors.each do |error|
 					error_msg[:desc] << error
 				end
 			end
 
-			error_msg.to_json
+			if halt_request
+				halt error_msg.to_json
+			else
+				error_msg.to_json
+			end
+
 		end
 	
 	end
